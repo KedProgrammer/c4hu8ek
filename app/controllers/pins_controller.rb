131522da03869f0ec72f1cf90e316bb4704f8192
Daniel Tobon <shiriux1@gmailcom>
2018-06-11 12:45:37 -0500

    verify token

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :check_token
 
   def index
     @pins = Pin.all.order('created_at DESC')
@@ -13,14 +14,20 @@ class PinsController < ApplicationController
     pin = Pin.new(pin_params)
     pin.user = current_user
     if pin.save
-      redirect_to root_path, notice: "El pin fue creado exitosamente"
+      redirect_to root_path, notice: 'El pin fue creado exitosamente'
     else
       render :new
     end
   end
 
   private
-    def pin_params
-      params.require(:pin).permit(:title, :image_url)
-    end
+
+  def pin_params
+    params.require(:pin).permit(:title, :image_url)
+  end
+
+  def check_token
+    render status: 401 unless (request.headers['X-User-Email'] == current_user.email) &&
+                              (request.headers['X-Api-Token'] == current_user.api_token)
+  end
 end
